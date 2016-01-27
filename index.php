4a289d1bd58856cb8228661a73b7b1b57b12f798
Francis <1111668@J-C222-OL-04.CRL.int>
2016-01-27 10:13:31 -0500

    Ajout du head et du css

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <html>
+<head>
+	<title>Garage à Denis</title>
+	<link rel="stylesheet" type="text/css" href="Style.css">
+	<script type="text/javascript" src="/1111668/js/Func.js"></script>
+	<meta charset="UTF-8">
+</head>
 <body>
-	test genial !!!
+ceci est un simple test
 </body>
 </html>
