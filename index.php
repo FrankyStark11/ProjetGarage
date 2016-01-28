8cc2888e7bfbeb6d066d6d81d3907c6793a41f11
Francis <1111668@J-C222-OL-04.CRL.int>
2016-01-28 11:07:15 -0500

    modification du design

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
-<!-- <?php echo "Projet : garage sur Web"; ?> -->
-<center>
-<br />
-<?php echo date('Y-m-d H:i:s');?>
+<!DOCTYPE HTML>
+<!--
+	Phase Shift by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
 <html>
-	<center>
 	<head>
-	<title>Garage à Denis</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-<?php
+		<title>Garage</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.dropotron.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-wide.css" />
+		</noscript>
+
+		<?php
 
 	// * * * Initialisation des GPIO * * *
 
@@ -93,74 +108,120 @@
 				}
 	}
 	?>
-
 	</head>
-
 	<body>
-		<br />
-		<?php
-		// Activation des portes
-		switch ($_GET["porte"]) {
-		case 1: // Active la porte 1
-			porte1();
-			break;
-		case 2: // Active la porte 2
-			porte2();
-			break;
-		case 3: // Active la porte 3
-			porte3();
-			break;
-		case 4: // Active la porte 4
-			porte4();
-			break;
-		case 5:  //active la fermeture des portes ouvertes
-			/*for ( $i = 25 ; $i <= 28 ; $i++ ) {
-			$porte = system ( "gpio read ".$i);
-			$gpio=$i-4;
-			if ($porte == "0") {
-			system ("gpio write ".$gpio." off");
-			sleep ( 1 );
-			system ("gpio write ".$gpio." on");
-			}
-			}*/
-			fermerToutesPortes();
-			break;
-		case 6; // arme|desarme email
-			$porte = system ("gpio read 1");
-			if ($porte == "0") {
-				system ("gpio write 1 1");
-			} else {
-				system ("gpio write 1 0");
-			}
-	}
-	?>
-	<br />
-	<div class="GARAGE">
-	<button class="dropbtn">ACTIVER</button>
-	<div class="garage-content">
-		<a href="index.php?porte=1">Porte 1</a>
-		<a href="index.php?porte=2">Porte 2</a>
-		<a href="index.php?porte=3">Porte 3</a>
-		<a href="index.php?porte=4">Porte 4</a>
-		<a href="index.php?porte=5">Fermer les portes</a>
-		<a href="index.php?porte=6">Alerte email</a>
-        </div>
+
+		<!-- Wrapper -->
+			<div class="wrapper style1">
+
+				<!-- Header -->
+					<div id="header" class="skel-panels-fixed">
+						<div id="logo">
+							<h1><a href="index.html">Systeme RPI</a></h1>
+						</div>
+					</div>
+
+				<!-- Extra -->
+					<div id="extra">
+						<div class="container">
+							<div class="row no-collapse-1">
+								<section class="4u"> 
+								<a href="#" class="image featured">
+								<?php
+								$porte = system ( "gpio read 25");
+								if ($porte == "0") {
+									?>   <img src="images/red.jpg" width=15%>
+									<?php  } else {
+									?>   <img src="images/green.jpg" width=15%>
+									<?php
+								}
+								?>
+
+								</a>
+									<div class="box">
+										<p>Porte # 1 <br> Etat : Fermer</p> 
+									</div>
+								</section>
+								<section class="4u"> <a href="#" class="image featured">
+									<?php
+								$porte = system ( "gpio read 26");
+								if ($porte == "0") {
+									?>   <img src="images/red.jpg" width=15%>
+									<?php  } else {
+									?>   <img src="images/green.jpg" width=15%>
+									<?php
+								}
+								?>
+								</a>
+									<div class="box">
+										<p>Porte # 2 <br> Etat : Fermer</p></div>
+								</section>
+								<section class="4u"> <a href="#" class="image featured">
+									<?php
+								$porte = system ( "gpio read 27");
+								if ($porte == "0") {
+									?>   <img src="images/red.jpg" width=15%>
+									<?php  } else {
+									?>   <img src="images/green.jpg" width=15%>
+									<?php
+								}
+								?>
+								</a>
+									<div class="box">
+										<p>Porte # 3 <br> Etat : Fermer</p></div>
+								</section>
+							</div>
+							<div class="row no-collapse-1">
+								<section class="4u"> <a href="#" class="image featured">
+									<?php
+								$porte = system ( "gpio read 28");
+								if ($porte == "0") {
+									?>   <img src="images/red.jpg" width=15%>
+									<?php  } else {
+									?>   <img src="images/green.jpg" width=15%>
+									<?php
+								}
+								?>
+								</a>
+									<div class="box">
+										<p>Porte # 4 <br> Etat : Fermer</p>
+								</section>
+								<section class="4u"> <a href="#" class="image featured">
+									<?php
+								$porte = system ( "gpio read 0");
+								if ($porte == "0") {
+									?>   <img src="images/red.jpg" width=15%>
+									<?php  } else {
+									?>   <img src="images/green.jpg" width=15%>
+									<?php
+								}
+								?>
+								</a>
+									<div class="box">
+										<p>Porte # 5 <br> Etat : Fermer</p>
+								</section>
+								<section class="4u"> <a href="#" class="image featured"><img src="images/garage.jpg" alt=""></a>
+									<div class="box">
+										<p>Toutes les portes</p>
+										<a href="#" class="button">Fermer</a></div>
+								</section>
+							</div>
+						</div>
+					</div>
 	</div>
-	<br />
-	<a href="index.php?porte=0">
-		<img src="Images/garage.jpg" width=90%>
-		<br />
-	</a>
-	<?php
-	// Lecture du statut des portes
-	statut();
-	$email = system ("gpio read 1") ;
-	if ($email == "1") {
-		?>
-		<img src="Images/email.jpg" width=10% >
-		<?php
-	}
-	?>
+	<!-- Copyright -->
+		<div id="copyright">
+			<div class="container">
+				<div class="copyright">
+					<p>Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)</p>
+					<ul class="icons">
+						<li><a href="#" class="fa fa-facebook"><span>Facebook</span></a></li>
+						<li><a href="#" class="fa fa-twitter"><span>Twitter</span></a></li>
+						<li><a href="#" class="fa fa-google-plus"><span>Google+</span></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
 	</body>
-	</center>
 </html>
