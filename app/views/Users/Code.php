91bac235f6b80c2e8bc86ce51b0f503c34c160a0
Francis Marsolais <FrancisMac@Mac-mini-de-Francis.local>
2016-02-02 13:30:55 -0500

    modification de la page de modification des mots de passe

--- app/views/Users/Code.php
+++ app/views/Users/Code.php
@@ -1,63 +1,48 @@
-<!DOCTYPE HTML>
-<!--
-	Phase Shift by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
--->
 <html>
 	<head>
 		<title>Garage</title>
+
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/jquery.dropotron.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-layers.min.js"></script>
-		<script src="js/init.js"></script>
-		<noscript>
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-wide.css" />
-		</noscript>
-		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+
+		<script src="js/javascript.js"></script>
+
+		<link rel="stylesheet" href="css/style2.css" />
+
 	</head>
-	<body>
-
-		<!-- Wrapper -->
-			<div class="wrapper style1">
-
-				<!-- Header -->
-					<div id="header" class="skel-panels-fixed">
-						<div id="logo">
-							<h1><a href="index.html">RPI</a></h1>
-						</div>
-					</div>
-				<!-- Header -->
-
-				<!-- Page -->
-					<div id="page" class="container">
-						<section>
-							<header class="major">
-								<h2>Entrez votre code</h2>
-								<img src="images/red.jpg" width=15%><img src="images/red.jpg" width=15%><img src="images/red.jpg" width=15%>
-							</header>
-							</section>
-					</div>
-				<!-- /Page -->			
-	</div>
-
-	<!-- Copyright -->
-		<div id="copyright">
-			<div class="container"> <span class="copyright">Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)</span>
-				<ul class="icons">
-					<li><a href="#" class="fa fa-facebook"><span>Facebook</span></a></li>
-					<li><a href="#" class="fa fa-twitter"><span>Twitter</span></a></li>
-					<li><a href="#" class="fa fa-google-plus"><span>Google+</span></a></li>
-				</ul>
-			</div>
-		</div>
-
-	</body>
+	<body onload="initialiser()">
+			<!-- code section -->	
+		<div class="CenterCode">
+			<form method="post" action="">
+				<table>
+					<tr>
+						<td>
+							<h2>Administration</h2>
+						</td>
+					</tr>
+					<tr>
+						<td><input class="TextMpd" type="text" placeholder="MPD"></td>
+					</tr>
+					<tr>
+						<td>
+							<h2>Proprio</h2>
+						</td>
+					</tr>
+					<tr>
+						<td><input class="TextMpd" type="text" placeholder="MPD"></td>
+					</tr>
+					<tr>
+						<td>
+							<h2>Ami</h2>
+						</td>
+					</tr>
+					<tr>
+						<td><input class="TextMpd" type="text" placeholder="MPD"></td>
+					</tr>
+					<tr>
+						<td><button class="Confirmer" type="button" name="4" value="Confirmer" onclick="feedPassword(this)"><span>Confimer </span></button></td>
+					</tr>
+				</table>
+			</form>
+		</div>	
+	</body >
 </html>
